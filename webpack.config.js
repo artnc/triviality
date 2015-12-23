3944d36996c48d68f2e9d596c73f281efc949ba2
Art Chaidarun <art@duolingo.com>
2015-12-22 21:31:15 -0500

    Install Babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ module.exports = {
   entry: [
     __dirname + '/src/index.js'
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
