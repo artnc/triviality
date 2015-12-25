63745fd228b544127ed9601854039ddd8c018aa2
Art Chaidarun <art@duolingo.com>
2015-12-25 14:36:23 -0500

    Log actions in debug mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import createLogger from 'redux-logger';
 import {convertApiChallengeToState} from './actions';
 import App from './containers/App';
 import app from './reducers';
 
+const middleWare = [];
+
+if (DEV) {
+  const logger = createLogger({
+    collapsed: true,
+    stateTransformer: (state) => state.toJS()
+  });
+  middleWare.push(logger);
+}
+
 const initialState = convertApiChallengeToState(window.initialData);
-const store = createStore(app, initialState);
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+const store = createStoreWithMiddleware(app, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
