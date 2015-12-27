9d8f8443e5d9821828f6475506bad62a9f072ec5
Art Chaidarun <art@duolingo.com>
2015-12-27 13:20:55 -0500

    Move store middleware code into own file

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {applyMiddleware, createStore} from 'redux';
-import createLogger from 'redux-logger';
-import {TILE_SELECT, initializeChallengeState, selectTile} from './actions';
+import {initializeChallengeState, selectTile} from './actions';
 import {GRID_HEIGHT, GRID_WIDTH} from './constants';
 import App from './containers/App';
 import rootReducer from './reducers';
+import createStoreWithMiddleware from './store';
 import './styles/global.scss';
 
 /* Initialize Redux */
 
-const middleWare = [];
-
-if (DEV) {
-  const HIDDEN_ACTIONS = [TILE_SELECT];
-  console.log(
-    `%caction logging disabled for: ${HIDDEN_ACTIONS.join(', ')}`,
-    'font-weight:700'
-  );
-  const logger = createLogger({
-    collapsed: true,
-    predicate: (getState, action) => !HIDDEN_ACTIONS.includes(action.type),
-    stateTransformer: (state) => state.toJS()
-  });
-  middleWare.push(logger);
-}
-
 const initialState = initializeChallengeState(window.initialData);
-const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
 const store = createStoreWithMiddleware(rootReducer, initialState);
 
 /* Initialize React */
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import {applyMiddleware, createStore} from 'redux';
+import createLogger from 'redux-logger';
+import {TILE_SELECT} from './actions';
+
+const middleWare = [];
+
+if (DEV) {
+  const HIDDEN_ACTIONS = [TILE_SELECT];
+  console.log(
+    `%caction logging disabled for: ${HIDDEN_ACTIONS.join(', ')}`,
+    'font-weight:700'
+  );
+  const logger = createLogger({
+    collapsed: true,
+    predicate: (getState, action) => !HIDDEN_ACTIONS.includes(action.type),
+    stateTransformer: (state) => state.toJS()
+  });
+  middleWare.push(logger);
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middleWare)(createStore);
+export default createStoreWithMiddleware;
