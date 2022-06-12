a706d64e3d961c59c7e5db7528e42f2e8a737907
Art Chaidarun <artchaidarun@gmail.com>
2022-06-11 21:17:21 -0400

    Avoid minifying built assets

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,16 +3,17 @@
 
 set -eu
 
-git checkout -b gh-pages
+git branch -D gh-pages || true
 docker run \
   -e NODE_ENV=production \
   -v "${PWD}:/code" \
   -v "/code/node_modules" \
   "$(docker build -q -t artnc/triviality .)" \
   node_modules/.bin/webpack -p
+git checkout -b gh-pages
 cp -a dist/. .
 git add -A
-git commit -m "Compile assets for GitHub Pages"
+git commit -m "Compile assets for GitHub Pages" -n
 git branch -u origin/gh-pages
 git push -f
 git checkout master
